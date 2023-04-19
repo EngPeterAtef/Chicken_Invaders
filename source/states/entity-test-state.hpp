dd52aba65996664dbed220660514422b6e222d41
markyasser <82395903+markyasser@users.noreply.github.com>
2023-04-19 23:39:10 +0200

    req 8

--- source/states/entity-test-state.hpp
+++ source/states/entity-test-state.hpp
@@ -45,7 +45,8 @@ class EntityTestState: public our::State {
         // Then we compute the VP matrix from the camera
         glm::ivec2 size = getApp()->getFrameBufferSize();
         //TODO: (Req 8) Change the following line to compute the correct view projection matrix 
-        glm::mat4 VP = glm::mat4(1.0f);
+        // glm::mat4 VP = glm::mat4(1.0f);
+        glm::mat4 VP = camera->getProjectionMatrix(size) * camera->getViewMatrix();     // will be multiplied by the object's model matrix
 
         for(auto& entity : world.getEntities()){
             // For each entity, we look for a mesh renderer (if none was found, we skip this entity)
@@ -53,6 +54,12 @@ class EntityTestState: public our::State {
             if(meshRenderer == nullptr) continue;
             //TODO: (Req 8) Complete the loop body to draw the current entity
             // Then we setup the material, send the transform matrix to the shader then draw the mesh
+            our::Material* material = meshRenderer->material;
+            material->setup();
+            material->shader->set("transform", VP * entity->getLocalToWorldMatrix());
+            //* Multiplying the model matrix of the specific entity VP to go to the canonical view volume.
+            
+            meshRenderer->mesh->draw();
         }
     }
 
