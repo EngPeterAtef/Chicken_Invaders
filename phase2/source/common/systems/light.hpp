291986a0fee17d159de567dc817992e2baa1cb07
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-05-21 13:36:33 +0300

    convert light direction to angles

--- phase2/source/common/systems/light.hpp
+++ phase2/source/common/systems/light.hpp
@@ -24,7 +24,8 @@ namespace our
                     world->lights[world->light_count].kind = light->kind;//directional light, point light, spot light
                     world->lights[world->light_count].position = glm::vec3(entity->getLocalToWorldMatrix() * glm::vec4(entity->localTransform.position, 1));//the position of the light in the world space
                     //the direction of the light in the world space (the roation in case of light represents the direction of the light)
-                    world->lights[world->light_count].direction = glm::normalize(glm::vec3(glm::transpose(glm::inverse(entity->getLocalToWorldMatrix())) * glm::vec4(entity->localTransform.rotation, 0)));
+                    world->lights[world->light_count].direction = glm::normalize(glm::vec3(entity->getLocalToWorldMatrix() * glm::vec4(0.0f, 1.0f, 0.0f, 0.0f)));
+                    // world->lights[world->light_count].direction = glm::normalize(glm::vec3(glm::transpose(glm::inverse(entity->getLocalToWorldMatrix())) * glm::vec4(entity->localTransform.rotation, 0)));
                     //we multiply by the transpose of the inverse of the local to world matrix to get the direction in the world space
                     //because the direction of the light is a vector normal to its surface
                     world->lights[world->light_count].color = light->color; //the color of the material in the light
