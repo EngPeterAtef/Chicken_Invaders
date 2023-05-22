079ef4b6a155987ed44d9e80db57c8ea274fe97a
markyasser <82395903+markyasser@users.noreply.github.com>
2023-05-22 15:12:09 +0300

    updated light

--- phase2/source/common/systems/light.hpp
+++ phase2/source/common/systems/light.hpp
@@ -21,17 +21,17 @@ namespace our
                 LightComponent *light = entity->getComponent<LightComponent>();
                 if (light)
                 {
-                    world->lights[world->light_count].kind = light->kind;//directional light, point light, spot light
-                    world->lights[world->light_count].position = glm::vec3(entity->getLocalToWorldMatrix() * glm::vec4(entity->localTransform.position, 1));//the position of the light in the world space
-                    //the direction of the light in the world space (the roation in case of light represents the direction of the light)
+                    world->lights[world->light_count].kind = light->kind;                                                            // directional light, point light, spot light
+                    world->lights[world->light_count].position = glm::vec3(entity->getLocalToWorldMatrix() * glm::vec4(0, 0, 0, 1)); // the position of the light in the world space
+                    // the direction of the light in the world space (the roation in case of light represents the direction of the light)
                     world->lights[world->light_count].direction = glm::normalize(glm::vec3(entity->getLocalToWorldMatrix() * glm::vec4(0.0f, 1.0f, 0.0f, 0.0f)));
                     // world->lights[world->light_count].direction = glm::normalize(glm::vec3(glm::transpose(glm::inverse(entity->getLocalToWorldMatrix())) * glm::vec4(entity->localTransform.rotation, 0)));
-                    //we multiply by the transpose of the inverse of the local to world matrix to get the direction in the world space
-                    //because the direction of the light is a vector normal to its surface
-                    world->lights[world->light_count].color = light->color; //the color of the material in the light
-                    world->lights[world->light_count].attenuation = light->attenuation;//the attenuation of the light
-                    world->lights[world->light_count].cone_angles = light->cone_angles;//the cone angles of the spot light
-                    world->light_count++;//number of lights in the world
+                    // we multiply by the transpose of the inverse of the local to world matrix to get the direction in the world space
+                    // because the direction of the light is a vector normal to its surface
+                    world->lights[world->light_count].color = light->color;             // the color of the material in the light
+                    world->lights[world->light_count].attenuation = light->attenuation; // the attenuation of the light
+                    world->lights[world->light_count].cone_angles = light->cone_angles; // the cone angles of the spot light
+                    world->light_count++;                                               // number of lights in the world
                 }
             }
         }
